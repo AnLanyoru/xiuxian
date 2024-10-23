09aa4e3e8f04de809e6127b4a5049c22515bccb4
AnLanyoru <AnLanyoru@outlook.com>
2024-10-24 04:23:31 +0800

    重置项目结构

--- xu/plugins/nonebot_plugin_xiuxian_2/xiuxian/xiuxian_impart_pk/__init__.py
+++ xu/plugins/nonebot_plugin_xiuxian_2/xiuxian/xiuxian_impart_pk/__init__.py
@@ -1,3 +1,5 @@
+import random
+
 from nonebot import on_command, require, on_fullmatch
 from nonebot.params import CommandArg
 from nonebot.adapters.onebot.v11 import (
@@ -23,15 +25,14 @@ xiuxian_impart = XIUXIAN_IMPART_BUFF()
 sql_message = XiuxianDateManage()  # sql类
 
 impart_re = require("nonebot_plugin_apscheduler").scheduler
-impart_pk_now = on_command("虚神界对决", priority=15, permission=GROUP, block=True)
-impart_pk_exp = on_command("虚神界闭关", aliases={"进入虚神界修炼", "闭关虚神界"},priority=8, permission=GROUP, block=True)
+impart_pk_now = on_command("虚神界对决", priority=3, permission=GROUP, block=True)
+impart_pk_exp = on_command("虚神界闭关", aliases={"进入虚神界修炼"}, priority=3, permission=GROUP, block=True)
 
 
 # 每日0点重置用虚神界次数
-@impart_re.scheduled_job("cron", hour=0, minute=0)
+@impart_re.scheduled_job("cron", hour=6, minute=0)
 async def impart_re_():
     impart_pk.re_data()
-    xu_world.re_data()
     logger.opt(colors=True).info(f"<green>已重置虚神界次数</green>")
 
 
@@ -44,49 +45,14 @@ async def impart_pk_now_(bot: Bot, event: GroupMessageEvent):
         await bot.send(event=event, message=msg)
         await impart_pk_now.finish()
     user_id = user_info['user_id']
-    sql_message.update_last_check_info_time(user_id)  # 更新查看修仙信息时间
-    impart_data_draw = await impart_pk_check(user_id)
-    if impart_data_draw is None:
-        msg = f"发生未知错误，多次尝试无果请找晓楠！"
+    pk_num = impart_pk.get_impart_pk_num(user_id)
+    if pk_num:
+        msg = f"道友今日已经对决过了，明天再来吧！"
         await bot.send(event=event, message=msg)
         await impart_pk_now.finish()
-    user_data = impart_pk.find_user_data(user_info['user_id'])
-
-    if user_data["pk_num"] <= 0:
-        msg = f"道友今日次数耗尽，明天再来吧！"
-        await bot.send(event=event, message=msg)
-        await impart_pk_now.finish()
-
-    player_1_stones = 0
-    player_2_stones = 0
-    combined_msg = ""
-    duel_count = 0
-    while user_data["pk_num"] > 0:
-        duel_count += 1
-        msg, win = await impart_pk_uitls.impart_pk_now_msg_to_bot(user_info['user_name'], NICKNAME)
-        if win == 1:
-            msg += f"战报：道友{user_info['user_name']}获胜,获得思恋结晶3颗\n"
-            impart_pk.update_user_data(user_info['user_id'], False)
-            xiuxian_impart.update_stone_num(3, user_id, 1)
-            player_1_stones += 3
-        elif win == 2:
-            msg += f"战报：道友{user_info['user_name']}败了,获得思恋结晶2颗\n"
-            impart_pk.update_user_data(user_info['user_id'], False)
-            xiuxian_impart.update_stone_num(2, user_id, 1)
-            player_1_stones += 2
-            if impart_pk.find_user_data(user_id)["pk_num"] <= 0 and xu_world.check_xu_world_user_id(user_id) is True:
-                msg += "检测到道友次数已用尽，已帮助道友退出虚神界！"
-                xu_world.del_xu_world(user_id)
-        else:
-            msg = f"挑战失败"
-            combined_msg += f"{msg}\n"
-            break
-
-        combined_msg += f"☆------------第{duel_count}次------------☆\n{msg}\n"
-        user_data = impart_pk.find_user_data(user_info['user_id'])
-
-    combined_msg += f"总计：道友{user_info['user_name']}获得思恋结晶{player_1_stones}颗\n"
-
+    stones = random.randint(6, 9)
+    xiuxian_impart.update_stone_num(stones, user_id, 1)
+    combined_msg = f"进入虚神界与{NICKNAME}对决，将{NICKNAME}打败{stones}次，获得思恋结晶{stones}颗\n"
     await bot.send(event=event, message=combined_msg)
     await impart_pk_now.finish()
 
